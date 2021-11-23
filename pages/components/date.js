a4d91f26abfa28045d6afbc302ca33186d93c3de
miaw629@gmail.com <miaw629@gmail.com>
2021-11-22 17:05:19 -0800

    add check for date

--- pages/components/date.js
+++ pages/components/date.js
@@ -2,5 +2,6 @@ import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+  if (date && dateString)
+    return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
 }
